75686dea7d6d4fc752ed0b75e51a8900d2157cbb
이정규 <dkssud6115@gmail.com>
2024-05-18 17:55:38 +0900

    blur 저리 가

--- app/components/layouts/NavBar.tsx
+++ app/components/layouts/NavBar.tsx
@@ -2,13 +2,7 @@ import Link from 'next/link';
 
 const NavBar = () => {
 	return (
-		<nav
-			className="fixed flex justify-between items-center h-16 w-full z-40 px-5 bg-white bg-opacity-50 shadow-md"
-			style={{
-				backdropFilter:
-					'blur(10px)' /* backdrop-filter를 적용하여 배경을 흐리게 만듭니다 */,
-			}}
-		>
+		<nav className="fixed flex justify-between items-center h-16 w-full z-40 px-5 bg-white shadow-md">
 			<Link href={'/'} className="flex items-center gap-4">
 				{/* TODO: Logo 제작하기 */}
 				<div className="w-10 h-10 bg-red-400 rounded-md" />
