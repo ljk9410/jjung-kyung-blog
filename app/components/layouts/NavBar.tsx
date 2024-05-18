ba3746e81f5ce99f962ee951f531c1a7fd878dbb
이정규 <dkssud6115@gmail.com>
2024-05-18 17:53:18 +0900

    backdrop blur 해결

--- app/components/layouts/NavBar.tsx
+++ app/components/layouts/NavBar.tsx
@@ -2,7 +2,13 @@ import Link from 'next/link';
 
 const NavBar = () => {
 	return (
-		<nav className="fixed flex justify-between items-center h-16 w-full z-40 px-5 bg-white bg-opacity-20 backdrop-blur-md shadow-md">
+		<nav
+			className="fixed flex justify-between items-center h-16 w-full z-40 px-5 bg-white bg-opacity-50 shadow-md"
+			style={{
+				backdropFilter:
+					'blur(10px)' /* backdrop-filter를 적용하여 배경을 흐리게 만듭니다 */,
+			}}
+		>
 			<Link href={'/'} className="flex items-center gap-4">
 				{/* TODO: Logo 제작하기 */}
 				<div className="w-10 h-10 bg-red-400 rounded-md" />
