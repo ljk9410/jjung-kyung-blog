9eeb7e10e1f621147dc3fe7becd5030c7280d87a
이정규 <dkssud6115@gmail.com>
2024-05-21 18:39:08 +0900

    아이콘 & 파비콘 적용

--- app/components/layouts/NavBar.tsx
+++ app/components/layouts/NavBar.tsx
@@ -1,15 +1,30 @@
 import Link from 'next/link';
 import DarkModeBtn from '../buttons/DarkModeBtn';
+import Image from 'next/image';
 
 const NavBar = () => {
   return (
-    <nav className="bg-white fixed z-40 flex h-16 w-full items-center justify-between px-5 shadow-md dark:bg-[#121212] dark:shadow-gray-600 dark:shadow-sm">
-      <Link href={'/'} className="flex items-center gap-4">
-        {/* TODO: Logo 제작하기 */}
-        <div className="bg-red-400 h-10 w-10 rounded-md" />
-        <span className="text-xl font-bold">JJung Kyung Blog</span>
-      </Link>
-      <DarkModeBtn />
+    <nav className="flex items-center justify-center bg-white h-16 fixed z-40 w-full shadow-md dark:bg-[#121212] dark:shadow-gray-600 dark:shadow-sm">
+      <div className="flex justify-between w-full max-w-[1080px] px-6">
+        <Link href={'/'} className="flex items-center gap-2 group">
+          <Image
+            width={60}
+            height={60}
+            src={'/peace-jjung-kyung.png'}
+            alt="main-logo"
+            className="group-hover:hidden"
+          />
+          <Image
+            width={60}
+            height={60}
+            src={'/idea-jjung-kyung.png'}
+            alt="main-logo"
+            className="hidden group-hover:block"
+          />
+          <span className="text-xl font-bold">쩡경의 Blog</span>
+        </Link>
+        <DarkModeBtn />
+      </div>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next';
 import './config/globals.css';
 import NavBar from './components/layouts/NavBar';
-import { ThemeProvider } from 'next-themes';
 import ThemeProviders from './components/layouts/ThemeProviders';
 import { inter } from './assets/fonts';
 
 export const metadata: Metadata = {
-  title: 'JJung Kyung Blog',
+  title: '쩡경의 Blog',
   description: 'JJung Kyung Blog: Leaving Footprints: Chronicles of My Life',
-  // icons (favicons)
+  icons: {
+    icon: '/develop-jjung-kyung.png',
+  },
 };
 
 export default function RootLayout({
